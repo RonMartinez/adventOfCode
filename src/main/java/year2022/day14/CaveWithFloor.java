57b9639b5a90bc1c1c9c3e6a1bfc96a1e228dca0
Ron Martinez <ron.martinez@gmail.com>
2022-12-14 08:53:22 -0600

    day 14 cleanup

--- src/main/java/year2022/day14/CaveWithFloor.java
+++ src/main/java/year2022/day14/CaveWithFloor.java
@@ -7,13 +7,12 @@ public class CaveWithFloor {
 	
 	public static final String ROCK = "#";
 	public static final String SAND = "o";
-	public static final Long MAX_ITERATIONS = 2_500L;
 
 	private Map<Coordinate, String> coordinateMap;
 	private Long maxY;
 	
 	public Coordinate dropSand(Coordinate sand) {
-		Coordinate coordinate = findSettledCoordinate(sand, 0L);
+		Coordinate coordinate = findSettledCoordinate(sand);
 		
 		if(coordinate != null) {
 			coordinateMap.put(coordinate, SAND);
@@ -22,31 +21,29 @@ public class CaveWithFloor {
 		return coordinate;
 	}
 
-	public Coordinate findSettledCoordinate(Coordinate sand, Long iterations) {
+	public Coordinate findSettledCoordinate(Coordinate sand) {
 		Coordinate coordinate = null;
 		
-		if(iterations < MAX_ITERATIONS) {
-			Long x = sand.getX();
-			Long y = sand.getY();
-			
-			Long newY = y+1;
-			
-			Coordinate down = new Coordinate(x, newY);
-			Coordinate downLeft = new Coordinate(x-1, newY);
-			Coordinate downRight = new Coordinate(x+1, newY);
-			if(newY < maxY) {
-				if(coordinateMap.get(down) == null) {
-					coordinate = findSettledCoordinate(down, iterations+1L);
-				} else if(coordinateMap.get(downLeft) == null) {
-					coordinate = findSettledCoordinate(downLeft, iterations+1L);
-				} else if(coordinateMap.get(downRight) == null) {
-					coordinate = findSettledCoordinate(downRight, iterations+1L);
-				} else {
-					coordinate = sand;
-				}
+		Long x = sand.getX();
+		Long y = sand.getY();
+		
+		Long newY = y+1;
+		
+		Coordinate down = new Coordinate(x, newY);
+		Coordinate downLeft = new Coordinate(x-1, newY);
+		Coordinate downRight = new Coordinate(x+1, newY);
+		if(newY < maxY) {
+			if(coordinateMap.get(down) == null) {
+				coordinate = findSettledCoordinate(down);
+			} else if(coordinateMap.get(downLeft) == null) {
+				coordinate = findSettledCoordinate(downLeft);
+			} else if(coordinateMap.get(downRight) == null) {
+				coordinate = findSettledCoordinate(downRight);
 			} else {
 				coordinate = sand;
 			}
+		} else {
+			coordinate = sand;
 		}
 	
 		return coordinate;
