7d65fef5f12e301f683756ddc327a9370b6b8a28
Ron Martinez <ron.martinez@gmail.com>
2022-12-18 09:33:27 -0600

    DimensionalCoordinate unit test

--- src/test/java/util/DimensionalCoordinateHelperTest.java
+++ src/test/java/util/DimensionalCoordinateHelperTest.java
@@ -1,39 +1,215 @@
 package util;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class DimensionalCoordinateHelperTest {
 	
 	@Test
-	public void test() {
+	public void test2dAdjacent() {
 		DimensionalCoordinate dimensionalCoordinate2d = new DimensionalCoordinate(Arrays.asList(0L, 0L));
-		Set<DimensionalCoordinate> adjacent = DimensionalCoordinateHelper.getAdjacentDimensionalCoordinates(dimensionalCoordinate2d);
-		Assertions.assertEquals(new HashSet<>(), adjacent);
+		Set<DimensionalCoordinate> actual = DimensionalCoordinateHelper.getAdjacentDimensionalCoordinates(dimensionalCoordinate2d);
+		Set<DimensionalCoordinate> expected = new HashSet<>(Arrays.asList(
+				new DimensionalCoordinate(Arrays.asList(-1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 1L))
+				));
+		assertEquals(expected, actual);
 		
-//		System.out.println("Adjacent");
-//		adjacent.forEach(dc -> System.out.println(dc));
-//		System.out.println();
-//		Set<DimensionalCoordinate> surrounding = DimensionalCoordinateHelper.getSurroundingDimensionalCoordinates(dimensionalCoordinate2d);
-//		System.out.println("Surrounding");
-//		surrounding.forEach(dc -> System.out.println(dc));
-//		System.out.println();
-//
-//		DimensionalCoordinate dimensionalCoordinate3d = new DimensionalCoordinate(Arrays.asList(0L, 0L));
-//		System.out.println(dimensionalCoordinate3d);
-//		Set<DimensionalCoordinate> adjacent3d = DimensionalCoordinateHelper.getAdjacentDimensionalCoordinates(dimensionalCoordinate3d);
-//		System.out.println("Adjacent");
-//		adjacent3d.forEach(dc -> System.out.println(dc));
-//		System.out.println();
-//		Set<DimensionalCoordinate> surrounding3d = DimensionalCoordinateHelper.getSurroundingDimensionalCoordinates(dimensionalCoordinate3d);
-//		System.out.println("Surrounding");
-//		
-//		surrounding3d.forEach(dc -> System.out.println(dc));
-//		System.out.println();
+	}
+	
+	@Test
+	public void test2dSurrounding() {
+		DimensionalCoordinate dimensionalCoordinate2d = new DimensionalCoordinate(Arrays.asList(0L, 0L));
+		Set<DimensionalCoordinate> actual = DimensionalCoordinateHelper.getSurroundingDimensionalCoordinates(dimensionalCoordinate2d);
+		Set<DimensionalCoordinate> expected = new HashSet<>(Arrays.asList(
+				new DimensionalCoordinate(Arrays.asList(-1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 0L)),
+				//new DimensionalCoordinate(Arrays.asList(0L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 1L))
+				));
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void test3dAdjacent() {
+		DimensionalCoordinate dimensionalCoordinate2d = new DimensionalCoordinate(Arrays.asList(0L, 0L, 0L));
+		Set<DimensionalCoordinate> actual = DimensionalCoordinateHelper.getAdjacentDimensionalCoordinates(dimensionalCoordinate2d);
+		Set<DimensionalCoordinate> expected = new HashSet<>(Arrays.asList(
+				new DimensionalCoordinate(Arrays.asList(-1L, 0L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 0L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, -1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 0L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 0L, 1L))
+				));
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void test3dSurrounding() {
+		DimensionalCoordinate dimensionalCoordinate2d = new DimensionalCoordinate(Arrays.asList(0L, 0L, 0L));
+		Set<DimensionalCoordinate> actual = DimensionalCoordinateHelper.getSurroundingDimensionalCoordinates(dimensionalCoordinate2d);
+		Set<DimensionalCoordinate> expected = new HashSet<>(Arrays.asList(
+				new DimensionalCoordinate(Arrays.asList(-1L, -1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, -1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, -1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 0L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 0L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 0L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 1L, -1L)),
+				
+				new DimensionalCoordinate(Arrays.asList(-1L, -1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, -1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(1L, -1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 0L, 0L)),
+				//new DimensionalCoordinate(Arrays.asList(0L, 0L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 0L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 1L, 0L)),
+
+				new DimensionalCoordinate(Arrays.asList(-1L, -1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, -1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, -1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 0L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 0L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 0L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 1L, 1L))
+				));
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void test4dAdjacent() {
+		DimensionalCoordinate dimensionalCoordinate2d = new DimensionalCoordinate(Arrays.asList(0L, 0L, 0L, 0L));
+		Set<DimensionalCoordinate> actual = DimensionalCoordinateHelper.getAdjacentDimensionalCoordinates(dimensionalCoordinate2d);
+		Set<DimensionalCoordinate> expected = new HashSet<>(Arrays.asList(
+				new DimensionalCoordinate(Arrays.asList(-1L, 0L, 0L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 0L, 0L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, -1L, 0L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 1L, 0L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 0L, -1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 0L, 1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 0L, 0L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 0L, 0L, 1L))
+				));
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void test4dSurrounding() {
+		DimensionalCoordinate dimensionalCoordinate2d = new DimensionalCoordinate(Arrays.asList(0L, 0L, 0L, 0L));
+		Set<DimensionalCoordinate> actual = DimensionalCoordinateHelper.getSurroundingDimensionalCoordinates(dimensionalCoordinate2d);
+		Set<DimensionalCoordinate> expected = new HashSet<>(Arrays.asList(
+				new DimensionalCoordinate(Arrays.asList(-1L, -1L, -1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, -1L, -1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, -1L, -1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 0L, -1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 0L, -1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 0L, -1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 1L, -1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 1L, -1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 1L, -1L, -1L)),
+				
+				new DimensionalCoordinate(Arrays.asList(-1L, -1L, 0L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, -1L, 0L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, -1L, 0L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 0L, 0L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 0L, 0L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 0L, 0L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 1L, 0L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 1L, 0L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 1L, 0L, -1L)),
+
+				new DimensionalCoordinate(Arrays.asList(-1L, -1L, 1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, -1L, 1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, -1L, 1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 0L, 1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 0L, 1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 0L, 1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 1L, 1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 1L, 1L, -1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 1L, 1L, -1L)),
+
+				new DimensionalCoordinate(Arrays.asList(-1L, -1L, -1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, -1L, -1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(1L, -1L, -1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 0L, -1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 0L, -1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 0L, -1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 1L, -1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 1L, -1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 1L, -1L, 0L)),
+				
+				new DimensionalCoordinate(Arrays.asList(-1L, -1L, 0L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, -1L, 0L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(1L, -1L, 0L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 0L, 0L, 0L)),
+				//new DimensionalCoordinate(Arrays.asList(0L, 0L, 0L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 0L, 0L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 1L, 0L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 1L, 0L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 1L, 0L, 0L)),
+
+				new DimensionalCoordinate(Arrays.asList(-1L, -1L, 1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, -1L, 1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(1L, -1L, 1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 0L, 1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 0L, 1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 0L, 1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 1L, 1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 1L, 1L, 0L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 1L, 1L, 0L)),
+
+				new DimensionalCoordinate(Arrays.asList(-1L, -1L, -1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, -1L, -1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, -1L, -1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 0L, -1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 0L, -1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 0L, -1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 1L, -1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 1L, -1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 1L, -1L, 1L)),
+				
+				new DimensionalCoordinate(Arrays.asList(-1L, -1L, 0L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, -1L, 0L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, -1L, 0L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 0L, 0L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 0L, 0L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 0L, 0L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 1L, 0L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 1L, 0L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 1L, 0L, 1L)),
+
+				new DimensionalCoordinate(Arrays.asList(-1L, -1L, 1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, -1L, 1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, -1L, 1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 0L, 1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 0L, 1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 0L, 1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(-1L, 1L, 1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(0L, 1L, 1L, 1L)),
+				new DimensionalCoordinate(Arrays.asList(1L, 1L, 1L, 1L))
+				));
+		assertEquals(expected, actual);
 	}
 
 }
