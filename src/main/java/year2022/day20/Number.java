1fe5c7b32a33431cb83eff8d09dcbd24db9849c2
Ron Martinez <ron.martinez@gmail.com>
2022-12-22 20:55:22 -0600

    day 20 wip

---
+++ src/main/java/year2022/day20/Number.java
@@ -0,0 +1,19 @@
+package year2022.day20;
+
+public class Number {
+	
+	private int value;
+	
+	public Number(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+}
